4ca4eaed9e46047c306698d65deb1f6bfad9dfa7
Eric Jeong <ericjeong@google.com>
2021-10-20 22:19:15 -0700

    Enlengthen VHAL health check interval in AA emulator
    - Car watchdog checks VHAL health every 3s.
    - Emulator VHAL can't respond within 3s sometimes due to emulator
    perforamnce issue.
    - The interval is set to 10s to avoid the crash.
    
    Bug: 193742550
    Test: VHAL should not be killed by car watchdog on AA emulator
    Change-Id: If8d6ceccad14d70e462d31c8a2efb93ce6c42d48

--- emulator/aosp_car_emulator.mk
+++ emulator/aosp_car_emulator.mk
@@ -34,7 +34,8 @@ PRODUCT_COPY_FILES += \
     device/generic/car/emulator/cluster/display_settings.xml:system/etc/display_settings.xml
 PRODUCT_PRODUCT_PROPERTIES += \
     hwservicemanager.external.displays=1,400,600,120,0 \
-    persist.service.bootanim.displays=8140900251843329
+    persist.service.bootanim.displays=8140900251843329 \
+    ro.carwatchdog.vhal_healthcheck.interval=10
 ifeq (true,$(ENABLE_CLUSTER_OS_DOUBLE))
 DEVICE_PACKAGE_OVERLAYS += device/generic/car/emulator/cluster/osdouble_overlay
 else
